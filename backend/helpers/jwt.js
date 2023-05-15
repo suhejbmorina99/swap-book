36d4978d9e115552f044803d7cdca8cbbdb27613
Suhejb <suhejbmorina@gmail.com>
2023-05-15 23:43:58 +0200

    playing with revoke functions

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,10 +2,11 @@ const { expressjwt: jwt } = require('express-jwt')
 
 function authJwt() {
     const secret = process.env.secret
-    const api = process.env.api
+    const api = process.env.API_URL
     return jwt({
         secret,
         algorithms: ['HS256'],
+        isRevoked: isRevoked,
     }).unless({
         path: [
             { url: /\/api\/v1\/book(.*)/, methods: ['GET', 'OPTIONS'] },
@@ -13,6 +14,16 @@ function authJwt() {
             `${api}/user/register`,
         ],
     })
+}
+
+async function isRevoked(req, payload) {
+    console.log(payload)
+    if (payload.isAdmin == false) {
+        console.log('Not Admin')
+        return true
+    }
+    console.log('Admin')
+    return false
 }
 
 module.exports = authJwt
