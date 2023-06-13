1aabea57e8df0a1a8dd3912fe124d028a229c60f
Suhejb <suhejbmorina@gmail.com>
2023-06-13 20:57:08 +0200

    added the actions for register form

--- frontend/src/app/store/actions/auth.actions.ts
+++ frontend/src/app/store/actions/auth.actions.ts
@@ -1,10 +1,14 @@
 import { createAction, props } from '@ngrx/store';
 import { SessionData } from '../reducers/auth.reducer';
+import { UserData } from '../reducers/auth.reducer';
 
 enum authActionsType {
   LOGIN = '[Auth] Login',
   SET_LOGIN_DATA = '[Auth] Login Success',
   LOGIN_FAIL = '[Auth] Login Fail',
+  REGISTER = '[Auth] Register',
+  SET_REGISTER_DATA = '[Auth] Register Success',
+  REGISTER_FAIL = '[Auth] Register Fail',
 }
 
 export const loginRequestAction = createAction(
@@ -19,5 +23,27 @@ export const setLoginDataAction = createAction(
 
 export const loginFailAction = createAction(
   authActionsType.LOGIN_FAIL,
+  props<{ message: string }>()
+);
+
+export const registerRequestAction = createAction(
+  authActionsType.REGISTER,
+  props<{
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    country: string;
+    city: string;
+  }>()
+);
+
+export const setRegisterData = createAction(
+  authActionsType.SET_REGISTER_DATA,
+  props<{ userData: UserData | undefined }>()
+);
+
+export const registerFailAction = createAction(
+  authActionsType.REGISTER_FAIL,
   props<{ message: string }>()
 );
