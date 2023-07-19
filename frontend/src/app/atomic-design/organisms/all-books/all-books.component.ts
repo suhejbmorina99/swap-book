2fbf7266e17742f9047d4085f9df257e0d9f8f3e
Suhejb <suhejbmorina@gmail.com>
2023-07-19 23:17:31 +0200

    redirect to swap just if we have books

--- frontend/src/app/atomic-design/organisms/all-books/all-books.component.ts
+++ frontend/src/app/atomic-design/organisms/all-books/all-books.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { BookServices } from 'src/app/store/services/book.services';
 
 @Component({
   selector: 'app-all-books',
   templateUrl: './all-books.component.html',
-  styleUrls: ['./all-books.component.scss']
+  styleUrls: ['./all-books.component.scss'],
 })
 export class AllBooksComponent {
-
-  public allBook: any[] = [];
+  public allBooks: any[] = [];
+  @Output() redirectToSwap = new EventEmitter<boolean>();
 
   constructor(private bookService: BookServices) {}
 
-
   ngOnInit() {
     this.bookService.getBooks().subscribe((data: any[]) => {
-      this.allBook = data;
+      this.allBooks = data;
+      if (this.allBooks.length > 0) {
+        this.redirectToSwap.emit(true);
+      }
     });
   }
 }
--- frontend/src/app/atomic-design/templates/main/main.component.ts
+++ frontend/src/app/atomic-design/templates/main/main.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
+  public redirectTo: boolean = false;
 
+  constructor(private router: Router) {}
+
+  navigateTo() {
+    if (this.redirectTo) {
+      this.router.navigate(['swap']);
+    }
+  }
+
+  onRedirectToSwap(redirectTo: boolean) {
+    this.redirectTo = redirectTo;
+  }
+
+  ngOnInit() {}
 }
