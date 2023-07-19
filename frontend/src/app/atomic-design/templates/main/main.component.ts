94435db6479371f5dec2380d389126a68387514c
Suhejb <suhejbmorina@gmail.com>
2023-07-19 23:21:36 +0200

    added the snackBar for adding a new book

--- frontend/src/app/atomic-design/templates/main/main.component.ts
+++ frontend/src/app/atomic-design/templates/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,11 +10,15 @@ import { Router } from '@angular/router';
 export class MainComponent {
   public redirectTo: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private snackBar: MatSnackBar) {}
 
   navigateTo() {
     if (this.redirectTo) {
       this.router.navigate(['swap']);
+    } else {
+      this.snackBar.open('Register new book, to be able to swap', undefined, {
+        duration: 2000,
+      });
     }
   }
 
