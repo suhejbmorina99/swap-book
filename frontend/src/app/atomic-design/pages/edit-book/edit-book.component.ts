20fadd123002c2a263a4f70e2f50914b03f5b702
Suhejb <suhejbmorina@gmail.com>
2023-07-25 23:11:47 +0200

    working in editing the book

--- frontend/src/app/atomic-design/pages/edit-book/edit-book.component.ts
+++ frontend/src/app/atomic-design/pages/edit-book/edit-book.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { BookServices } from 'src/app/store/services/book.services';
 
 @Component({
   selector: 'app-edit-book',
   templateUrl: './edit-book.component.html',
-  styleUrls: ['./edit-book.component.scss']
+  styleUrls: ['./edit-book.component.scss'],
 })
 export class EditBookComponent {
+  public allBooks: any[] = [];
 
+  constructor(private bookService: BookServices) {}
+
+  ngOnInit() {
+    const userId = localStorage.getItem('userId');
+    if (userId) {
+      this.bookService.getUserBooks({ id: userId }).subscribe((data: any[]) => {
+        this.allBooks = data;
+      });
+    }
+  }
 }
