dfdbec658fb17873740503239406ca388c2dee2a
Suhejb <suhejbmorina@gmail.com>
2023-10-21 17:57:44 +0200

    start working on new component

--- frontend/src/app/atomic-design/organisms/ready-to-swap/ready-to-swap.component.ts
+++ frontend/src/app/atomic-design/organisms/ready-to-swap/ready-to-swap.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookServices } from 'src/app/store/services/book.services';
 
 @Component({
   selector: 'app-ready-to-swap',
   templateUrl: './ready-to-swap.component.html',
-  styleUrls: ['./ready-to-swap.component.scss']
+  styleUrls: ['./ready-to-swap.component.scss'],
 })
 export class ReadyToSwapComponent {
+  public userBooks: any[] = [];
+  
+  constructor(private bookService: BookServices, private router: Router) {}
 
+  ngOnInit() {
+    const userId = localStorage.getItem('userId');
+    if (userId) {
+      this.bookService.getUserBooks({ id: userId }).subscribe((data: any[]) => {
+        this.userBooks = data;
+        console.log(data);
+      });
+    }
+  }
 }
