ad0ecadd11e7348c345c1202e4bf62d8eaa0620d
Suhejb <suhejbmorina@gmail.com>
2023-10-29 22:22:16 +0100

    working in filter options

--- frontend/src/app/atomic-design/organisms/filter/filter.component.ts
+++ frontend/src/app/atomic-design/organisms/filter/filter.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { category } from 'src/shared/stores/categories-data.store';
+import { language } from 'src/shared/stores/language-store';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent {
+  public categorySource: any = category;
+  public languageSource: any = language;
 
+  category = new FormControl('');
+  language = new FormControl('');
 }
