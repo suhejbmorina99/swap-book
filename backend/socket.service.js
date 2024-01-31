dfb96f0bf01b2ae39404784078177527d9a3a413
Suhejb <suhejbmorina@gmail.com>
2024-01-31 22:49:07 +0100

    integrated the swapRequest in the websocket service

--- backend/socket.service.js
+++ backend/socket.service.js
@@ -1,25 +1,34 @@
 // socket.service.js
-const socketIo = require('socket.io');
+const socketIo = require('socket.io')
 
 function initializeSocket(server) {
-  const io = socketIo(server, { cors: { origin: '*' } });
+    const io = socketIo(server, { cors: { origin: '*' } })
 
-  io.on('connection', (socket) => {
-    console.log('A user connected');
+    io.on('connection', (socket) => {
+        console.log('A user connected')
 
-    // You can add socket.io event handlers here
-    // For example, to handle a book created event:
-    socket.on('book created', (book) => {
-      console.log('Book Created:', book);
-      io.emit('book created', book);
-    });
+        // Handle swapRequest event
+        socket.on('swapRequest', (data) => {
+            console.log('Received swap request:', data)
 
-    socket.on('disconnect', () => {
-      console.log('A user disconnected');
-    });
-  });
+            // Emit swapRequest event to the other user
+            const otherUserId = data.otherUserId // Replace with the actual user ID
+            io.to(otherUserId).emit('swapRequest', data)
+        })
 
-  return io;
+        // You can add socket.io event handlers here
+        // For example, to handle a book created event:
+        socket.on('book created', (book) => {
+            console.log('Book Created:', book)
+            io.emit('book created', book)
+        })
+
+        socket.on('disconnect', () => {
+            console.log('A user disconnected')
+        })
+    })
+
+    return io
 }
 
-module.exports = { initializeSocket };
+module.exports = { initializeSocket }
